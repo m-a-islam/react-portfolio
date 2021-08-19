66e886cef82ed1d945e6521984498e79cba386f3
muhammad <muhammad.cse36@gmail.com>
2021-08-19 02:58:02 +0200

    menu added and transition

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Portfolio from "./components/portfolio/Portfolio";
 import "./app.scss";
+import { useState } from "react";
+import Menu from "./components/menu/Menu";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-      <Topbar/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro/>
         <Portfolio/>
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,7 +2,7 @@ import "./testimonial.scss"
 
 export default function Testimonials() {
     return (
-        <div className="testimonials">
+        <div className="testimonials" id="testimonials">
             
         </div>
     )
