68052e42087583e9c6164980dfe73d4164256310
muhammad <muhammad.cse36@gmail.com>
2021-08-22 01:46:52 +0200

    portfolio wip

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,15 +5,41 @@ export default function Portfolio() {
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
             <ul>
-                <li>Featured</li>
-                <li>Featured</li>
-                <li>Featured</li>
-                <li>Featured</li>
-                <li>Featured</li>
+                <li className="active">Featured</li>
+                <li>Web Application</li>
+                <li>Machine Learning</li>
+                <li>Big Data</li>
+                <li>Cloud Computing</li>
+                <li>Security and Cryptography</li>
             </ul>
             <div className="container">
                 <div className="item">
-                    
+                    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.interoffice.de%2Finfrastrukturprojekte%2Feinwands-und-erwiderungsmanagement%2F&psig=AOvVaw3CYdtXjnxKTCv3HO0IPemq&ust=1629675393791000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJDGy4ukw_ICFQAAAAAdAAAAABAa" alt="" />
+                    <h3>EWM</h3>
+                </div>
+                <div className="item">
+                    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.interoffice.de%2Finfrastrukturprojekte%2Feinwands-und-erwiderungsmanagement%2F&psig=AOvVaw3CYdtXjnxKTCv3HO0IPemq&ust=1629675393791000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJDGy4ukw_ICFQAAAAAdAAAAABAa" alt="" />
+                    <h3>EWM</h3>
+                </div>
+                <div className="item">
+                    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.interoffice.de%2Finfrastrukturprojekte%2Feinwands-und-erwiderungsmanagement%2F&psig=AOvVaw3CYdtXjnxKTCv3HO0IPemq&ust=1629675393791000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJDGy4ukw_ICFQAAAAAdAAAAABAa" alt="" />
+                    <h3>EWM</h3>
+                </div>
+                <div className="item">
+                    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.interoffice.de%2Finfrastrukturprojekte%2Feinwands-und-erwiderungsmanagement%2F&psig=AOvVaw3CYdtXjnxKTCv3HO0IPemq&ust=1629675393791000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJDGy4ukw_ICFQAAAAAdAAAAABAa" alt="" />
+                    <h3>EWM</h3>
+                </div>
+                <div className="item">
+                    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.interoffice.de%2Finfrastrukturprojekte%2Feinwands-und-erwiderungsmanagement%2F&psig=AOvVaw3CYdtXjnxKTCv3HO0IPemq&ust=1629675393791000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJDGy4ukw_ICFQAAAAAdAAAAABAa" alt="" />
+                    <h3>EWM</h3>
+                </div>
+                <div className="item">
+                    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.interoffice.de%2Finfrastrukturprojekte%2Feinwands-und-erwiderungsmanagement%2F&psig=AOvVaw3CYdtXjnxKTCv3HO0IPemq&ust=1629675393791000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJDGy4ukw_ICFQAAAAAdAAAAABAa" alt="" />
+                    <h3>EWM</h3>
+                </div>
+                <div className="item">
+                    <img src="https://98webhost.net/wp-content/uploads/2021/04/top-4-enterprise-application-integration-benefits.jpg" alt="" />
+                    <h3>EWM</h3>
                 </div>
             </div>
         </div>
