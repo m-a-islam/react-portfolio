c7249fd5a586e664ab93e8291ce46c807611874f
muhammad <muhammad.cse36@gmail.com>
2022-09-11 16:11:36 +0200

    function and const with jsx

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-ReactDOM.render(<h1>Test</h1>, document.getElementById("root"))
+
+function MainContent() {
+    return (
+        <h1> Bismillahir-Rahmanir-Rahim</h1>
+    )
+}
+
+function UnorderList() {
+    return (
+        <ul>
+            <li>Pricing</li>
+            <li>about</li>
+            <li>Contact</li>
+        </ul>
+    )
+}
+const page = (
+    <div>
+        <h1>My Awsome website in React</h1>
+        <h3>Reason I love React</h3>
+        <ol>
+            <li>Hello</li>
+            <li>There</li>
+            <li>thee</li>
+        </ol>
+    </div>
+)
+ReactDOM.render(
+    page,
+    document.getElementById("root")
+)
